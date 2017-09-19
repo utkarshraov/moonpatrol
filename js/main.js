ac52c27015f91a4988776d9da40e103c581f50b7
u1148646 <utkarshrao.viswavasu@utah.edu>
2017-09-19 09:09:53 -0600

    Initial commit. Contains basic logic for spawning enemies, collisions, player movement and jumping

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var game = new Phaser.Game(640,360,Phaser.Auto, '',{preload: preload, create: create, update: update});
+
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+	
+}
